7d5c85aeaf959b869554d068f8725d7a80b944e7
yangjiakai <2439576297@qq.com>
2023-02-01 22:00:00 +0800

    刷新了

---
+++ run.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+def main():
+    os.system("cd ~/文档/cornGetIP/")
+    os.system("curl cip.cc |grep IP >> dizhi.txt")
+    os.system("git add *")
+    os.system("git commit -m '刷新了'")
+    os.system("git push")
+    time.sleep(1)
+    os.system("1")
+
+
+
+
+if __name__ == "__main__":
+    main()
